dc6a7f136eefc423270c56e1536028de7af4a9aa
kabinja <renaudrwemakika@gmail.com>
2020-11-17 11:56:46 +0100

    adding more test that rely on System to see how to instrument it

--- src/test/java/org/example/ThreadUtilsTest.java
+++ src/test/java/org/example/ThreadUtilsTest.java
@@ -4,13 +4,25 @@ import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayOutputStream;
 
+import static java.time.Duration.ofSeconds;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ThreadUtilsTest {
     @Test
-    void testUnsafeThreatWithTimeout() throws InterruptedException {
+    void testUnsafeThreatWithSleep() throws InterruptedException {
         final ByteArrayOutputStream stream = ThreadUtils.toStreamUnsafe("Hello ", "World");
-        Thread.sleep(500);
+        Thread.sleep(200);
         assertEquals("Hello World", stream.toString());
+    }
+
+    @Test
+    void threadWithTimeout()
+    {
+
+        assertTimeout(ofSeconds(5), () -> {
+            final ByteArrayOutputStream stream = ThreadUtils.toStreamUnsafe("Hello ", "World");
+            Thread.sleep(200);
+            assertEquals("Hello World", stream.toString());
+        });
     }
 }
