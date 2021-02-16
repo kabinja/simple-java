36f10ce1427e4a4a0eb204971146fa729f501d12
Adriano FRANCI <adriano@franci.lu>
2021-02-16 17:47:51 +0100

    added plugin config

--- src/test/java/org/example/MathUtilsTest.java
+++ src/test/java/org/example/MathUtilsTest.java
@@ -2,18 +2,43 @@ package org.example;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.net.URLPermission;
+import java.util.Random;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MathUtilsTest {
     @Test
     void testInflateWithPositiveNumber(){
         double value = 1.;
+
+        Random rnd = new Random();
+
+
+        final ByteArrayOutputStream stream = ThreadUtils.toStreamUnsafe("Hello ", "World");
+        Thread t = new Thread(() -> assertEquals(stream.toString(), "Hello world"));
+
+        for (double i = 0.0 ; true; i = rnd.nextDouble()){
+            if (i>0.9) {
+                t.start();
+                break;
+            }
+        }
+
         assertTrue(value < MathUtils.inflate(value));
+
     }
 
     @Test
-    void testInflateWithNegativeNumber(){
+    void testInflateWithNegativeNumber() throws Exception{
         double value = -1.;
+
+
+        try {
+            if(Math.random() > 1) throw new IllegalStateException("Test exception");
+        }catch(Exception ignored){}
+
         assertTrue(value > MathUtils.inflate(value));
     }
 
