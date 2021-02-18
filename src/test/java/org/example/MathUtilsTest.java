ce722910dcb50d72f57a77bd8734c8f72a098731
Adriano FRANCI <adriano@franci.lu>
2021-02-18 18:36:43 +0100

    TEst update to trigger initial "inconsistent stack height" bug

--- src/test/java/org/example/MathUtilsTest.java
+++ src/test/java/org/example/MathUtilsTest.java
@@ -9,42 +9,51 @@ import java.util.Random;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MathUtilsTest {
+//    @Test
+//    void testInflateWithPositiveNumber(){
+//        double value = 1.;
+//
+//        Random rnd = new Random();
+//
+//
+//        final ByteArrayOutputStream stream = ThreadUtils.toStreamUnsafe("Hello ", "World");
+//        Thread t = new Thread(() -> assertEquals(stream.toString(), "Hello world"));
+//
+//        for (double i = 0.0 ; true; i = rnd.nextDouble()){
+//            if (i>0.9) {
+//                t.start();
+//                break;
+//            }
+//        }
+//
+//        assertTrue(value < MathUtils.inflate(value));
+//
+//    }
+
     @Test
-    void testInflateWithPositiveNumber(){
-        double value = 1.;
+    void testInflateWithNegativeNumber() {
+        final String[] args = new String[] { "-f=bar" };
 
-        Random rnd = new Random();
+        final StringBuilder options = new StringBuilder();
+        options.append(args);
 
-
-        final ByteArrayOutputStream stream = ThreadUtils.toStreamUnsafe("Hello ", "World");
-        Thread t = new Thread(() -> assertEquals(stream.toString(), "Hello world"));
-
-        for (double i = 0.0 ; true; i = rnd.nextDouble()){
-            if (i>0.9) {
-                t.start();
-                break;
-            }
+        try
+        {
+//            double r = Math.random();
+//            if(r > 0){
+////                throw new Exception("");
+//            }
+            assertTrue(true);
         }
-
-        assertTrue(value < MathUtils.inflate(value));
-
+        catch (final Exception e)
+        {
+            assertTrue(true);
+        }
     }
 
-    @Test
-    void testInflateWithNegativeNumber() throws Exception{
-        double value = -1.;
-
-
-        try {
-            if(Math.random() > 1) throw new IllegalStateException("Test exception");
-        }catch(Exception ignored){}
-
-        assertTrue(value > MathUtils.inflate(value));
-    }
-
-    @Test
-    void testInflateWithZero(){
-        double value = 0.;
-        assertEquals(0., MathUtils.inflate(value));
-    }
+//    @Test
+//    void testInflateWithZero(){
+//        double value = 0.;
+//        assertEquals(0., MathUtils.inflate(value));
+//    }
 }
